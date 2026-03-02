044cc58a489a6723bb7b4cf44ed924ec97c41f61
serenileonardo <serenileonardo.web@gmail.com>
2026-03-02 16:30:28 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,11 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import AddTrip from "./pages/AddTrip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-//import layout
 import DefaultLayout from "./layouts/DefaultLayouts";
 
 function App() {
   return (
-    <div className="d-flex flex-column min-vh-100">
-
-      <Header />
-
-      <div className="flex-grow-1">
+    
         <BrowserRouter>
           <Routes>
             <Route element={<DefaultLayout />}>
@@ -21,13 +15,7 @@ function App() {
             </Route>
           </Routes>
         </BrowserRouter>
-      </div>
-
-      <footer className="bg-primary text-white text-center py-3">
-        Travel
-      </footer>
-
-    </div>
+  
   );
 }
 
