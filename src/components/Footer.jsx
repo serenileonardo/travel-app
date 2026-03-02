f344341f3bbeb01292421eb7283e89be1135d023
serenileonardo <serenileonardo.web@gmail.com>
2026-03-02 16:26:05 +0100

    fix: clean code

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Footer() {
-    return (
-        <footer className="bg-primary text-white text-center py-3 mt-auto">
-            <p className="m-0">Travel</p>
-        </footer>
-    );
-}
