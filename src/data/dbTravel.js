28f44e6b2ffcd01aa4a385edb1770a34609e6fbe
Luca Striano <luca.striano90@gmail.com>
2026-03-02 15:00:56 +0100

    modify data db

--- src/data/dbTravel.js
+++ src/data/dbTravel.js
@@ -1,50 +1,80 @@
 const dbTravel = [
   {
-    nome: "Luca",
-    cognome: "Rossi",
-    email: "luca.rossi@email.com",
-    codiceFiscale: "RSSLCU90A01H501Z",
-    numeroCliente: "+393401234567",
-    numeroEmergenza: "+393201112233",
-    viaggio: "Roma"
+    "id": 1,
+    "localita": "Roma",
+    "descrizione": "Weekend culturale alla scoperta dei monumenti storici e della cucina tipica romana.",
+    "dataInizio": "2026-04-10",
+    "dataFine": "2026-04-13",
+    "partecipanti": [
+      {
+        "id": 101,
+        "nome": "Luca",
+        "cognome": "Bianchi",
+        "email": "luca.bianchi@email.com",
+        "telefono": "+39 333 1234567",
+        "dataNascita": "1990-05-14"
+      },
+      {
+        "id": 102,
+        "nome": "Sara",
+        "cognome": "Rossi",
+        "email": "sara.rossi@email.com",
+        "telefono": "+39 347 9876543",
+        "dataNascita": "1992-08-21"
+      }
+    ]
   },
   {
-    nome: "Giulia",
-    cognome: "Bianchi",
-    email: "giulia.bianchi@email.com",
-    codiceFiscale: "BNCGLI92B41F205X",
-    numeroCliente: "+393491234568",
-    numeroEmergenza: "+393201112244",
-    viaggio: "Londra"
+    "id": 2,
+    "localita": "Parigi",
+    "descrizione": "Viaggio romantico tra musei, passeggiate lungo la Senna e visita alla Torre Eiffel.",
+    "dataInizio": "2026-06-01",
+    "dataFine": "2026-06-07",
+    "partecipanti": [
+      {
+        "id": 201,
+        "nome": "Marco",
+        "cognome": "Verdi",
+        "email": "marco.verdi@email.com",
+        "telefono": "+39 320 1122334",
+        "dataNascita": "1988-11-03"
+      }
+    ]
   },
   {
-    nome: "Marco",
-    cognome: "Verdi",
-    email: "marco.verdi@email.com",
-    codiceFiscale: "VRDMRC88C12D612P",
-    numeroCliente: "+393471234569",
-    numeroEmergenza: "+393201112255",
-    viaggio: "Parigi"
-  },
-  {
-    nome: "Sara",
-    cognome: "Conti",
-    email: "sara.conti@email.com",
-    codiceFiscale: "CNTSRA95D53G273K",
-    numeroCliente: "+393451234570",
-    numeroEmergenza: "+393201112266",
-    viaggio: "Zurigo"
-  },
-  {
-    nome: "Alessandro",
-    cognome: "Ferrari",
-    email: "alessandro.ferrari@email.com",
-    codiceFiscale: "FRRLSN91E14L219Q",
-    numeroCliente: "+393431234571",
-    numeroEmergenza: "+393201112277",
-    viaggio: "Barcellona"
+    "id": 3,
+    "localita": "New York",
+    "descrizione": "Tour metropolitano tra grattacieli, Central Park e spettacoli a Broadway.",
+    "dataInizio": "2026-09-15",
+    "dataFine": "2026-09-22",
+    "partecipanti": [
+      {
+        "id": 301,
+        "nome": "Giulia",
+        "cognome": "Ferrari",
+        "email": "giulia.ferrari@email.com",
+        "telefono": "+39 349 5566778",
+        "dataNascita": "1995-02-10"
+      },
+      {
+        "id": 302,
+        "nome": "Alessandro",
+        "cognome": "Conti",
+        "email": "alessandro.conti@email.com",
+        "telefono": "+39 328 4455667",
+        "dataNascita": "1991-07-18"
+      },
+      {
+        "id": 303,
+        "nome": "Elena",
+        "cognome": "Gallo",
+        "email": "elena.gallo@email.com",
+        "telefono": "+39 335 7788990",
+        "dataNascita": "1993-12-05"
+      }
+    ]
   }
-];
+]
 
 
 export default dbTravel;
