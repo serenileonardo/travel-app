580e423e4d5ebb95919d604d2b2ca3b610a21f3a
serenileonardo <serenileonardo.web@gmail.com>
2026-03-02 11:40:27 +0100

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddTrip from "./pages/AddTrip";
+import Navbar from "./components/Navbar";
+
+
 
 function App() {
+  return (
+    <>
+      <Navbar />
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-trip" element={<AddTrip />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
