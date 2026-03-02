f8d5e78bfe5bbf95ab27ec97e7fd0339fddcfa85
Daniel Pio Falabella <danielpiofalabella001@gmail.com>
2026-03-02 12:27:36 +0100

    creazione db Travel

---
+++ src/data/dbTravel.js
@@ -0,0 +1,47 @@
+const dbTravel = [
+  {
+    nome: "Luca",
+    cognome: "Rossi",
+    email: "luca.rossi@email.com",
+    codiceFiscale: "RSSLCU90A01H501Z",
+    numeroCliente: "+393401234567",
+    numeroEmergenza: "+393201112233",
+    viaggio: "Roma"
+  },
+  {
+    nome: "Giulia",
+    cognome: "Bianchi",
+    email: "giulia.bianchi@email.com",
+    codiceFiscale: "BNCGLI92B41F205X",
+    numeroCliente: "+393491234568",
+    numeroEmergenza: "+393201112244",
+    viaggio: "Londra"
+  },
+  {
+    nome: "Marco",
+    cognome: "Verdi",
+    email: "marco.verdi@email.com",
+    codiceFiscale: "VRDMRC88C12D612P",
+    numeroCliente: "+393471234569",
+    numeroEmergenza: "+393201112255",
+    viaggio: "Parigi"
+  },
+  {
+    nome: "Sara",
+    cognome: "Conti",
+    email: "sara.conti@email.com",
+    codiceFiscale: "CNTSRA95D53G273K",
+    numeroCliente: "+393451234570",
+    numeroEmergenza: "+393201112266",
+    viaggio: "Zurigo"
+  },
+  {
+    nome: "Alessandro",
+    cognome: "Ferrari",
+    email: "alessandro.ferrari@email.com",
+    codiceFiscale: "FRRLSN91E14L219Q",
+    numeroCliente: "+393431234571",
+    numeroEmergenza: "+393201112277",
+    viaggio: "Barcellona"
+  }
+];
